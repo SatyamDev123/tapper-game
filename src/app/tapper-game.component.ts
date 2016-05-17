bfe40dd744a744bd9de4a796fc96d021880d1dd1
satyam dev <satyamdev@satyams-MacBook-Air.local>
2016-05-17 14:08:59 +0530

    fixed firebase live player score

--- src/app/tapper-game.component.ts
+++ src/app/tapper-game.component.ts
@@ -11,52 +11,86 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 
 export class TapperGameAppComponent {
-  results: FirebaseListObservable<any[]>;
-  key: string;
+  timers: FirebaseListObservable<any[]>;
+  player1_scores: FirebaseListObservable<any[]>;
+  player2_scores: FirebaseListObservable<any[]>;
+  timer_key: string;
+  player1_key: string;
+  player2_key: string;
 
-  result = {
+  timer = {
     timerProgress: 10,
-    score: {
-      player1:0,
-      player2:0
-    },
     match_result: '',
     active: false,
     restart_btn_toggle: false
   };
 
+  player1 = {
+    score: 0
+  };
+  player2 = {
+    score: 0
+  };
+
 
   constructor(af: AngularFire) {
-    this.results = af.list('/results/');
-    this.results._ref.on("child_added", (snapshot)=> {
-      this.key = snapshot.key();
+    this.timers = af.list('/timer/');
+    this.player1_scores = af.list('/player1/');
+    this.player2_scores = af.list('/player2/');
+
+    this.timers._ref.on("child_added", (snapshot)=> {
+      this.timer_key = snapshot.key();
     });
-    this.results._ref.on("child_changed", (snapshot)=> {
-      let changed_result = snapshot.val();
-      this.result = changed_result;
+    this.timers._ref.on("child_changed", (snapshot)=> {
+      let changed_timer_result = snapshot.val();
+      this.timer = changed_timer_result;
+    });
+
+    this.player1_scores._ref.on("child_added", (snapshot)=> {
+      this.player1_key = snapshot.key();
+      this.player1 = {
+        score: 0
+      };
+    });
+    this.player2_scores._ref.on("child_added", (snapshot)=> {
+      this.player2_key = snapshot.key();
+      this.player2 = {
+        score: 0
+      };
+    });
+
+    this.player1_scores._ref.on("child_changed", (snapshot)=> {
+      let changed_player1_result = snapshot.val();
+      this.player1 = changed_player1_result;
+    });
+    this.player2_scores._ref.on("child_changed", (snapshot)=> {
+      let changed_player2_result = snapshot.val();
+      this.player2 = changed_player2_result;
     });
   }
 
   tapLeft() {
-    if(this.key) {
-      this.result.score.player1++;
-      this.results.update(this.key, this.result);
+    if(this.player1_key) {
+      this.player1.score++;
+      this.player1_scores.update(this.player1_key, this.player1);
     }
   }
   tapRight() {
-    if(this.key){
-      this.result.score.player2++;
-      this.results.update(this.key, this.result);
+    if(this.player2_key){
+      this.player2.score++;
+      this.player2_scores.update(this.player2_key, this.player2);
     }
   }
 
   restartGame() {
-    this.result = {
+    this.player1 = {
+      score: 0
+    };
+    this.player2 = {
+      score: 0
+    };
+    this.timer = {
       timerProgress: 10,
-      score: {
-        player1:0,
-        player2:0
-      },
       match_result: '',
       active: true,
       restart_btn_toggle: false
@@ -65,26 +99,26 @@ export class TapperGameAppComponent {
   }
 
   startGame() {
-    this.result.active = true;
-    this.results.push(this.result).then((snapshot)=>{
-      const inteval = setInterval(()=>{
-            if(this.result.timerProgress === 0){
-              clearInterval(inteval);
-              let score = this.result.score;
-              this.result.restart_btn_toggle=true;
-              if(score.player1 > score.player2) {
-                this.result.match_result = 'Player 1 Won the match';
-              } else if (score.player1 < score.player2) {
-                this.result.match_result = 'Player 2 Won the match';
-              }else if (score.player1 === score.player2) {
-                this.result.match_result = 'Match Draw';
-              }
-              this.results.update(this.key, this.result);
-            } else {
-              this.result.timerProgress--;
-              this.results.update(this.key, this.result);
+    this.timer.active = true;
+    this.timers.push(this.timer);
+    this.player1_scores.push(this.player1);
+    this.player2_scores.push(this.player2);
+    const inteval = setInterval(()=>{
+          if(this.timer.timerProgress === 0){
+            clearInterval(inteval);
+            this.timer.restart_btn_toggle=true;
+            if(this.player1.score > this.player2.score) {
+              this.timer.match_result = 'Player 1 Won the match';
+            } else if (this.player1.score < this.player2.score) {
+              this.timer.match_result = 'Player 2 Won the match';
+            } else if (this.player1.score === this.player2.score) {
+              this.timer.match_result = 'Match Draw';
             }
-      },1000);
-    });
+            this.timers.update(this.timer_key, this.timer);
+          } else {
+            this.timer.timerProgress--;
+            this.timers.update(this.timer_key, this.timer);
+          }
+    },1000);
   }
 }
